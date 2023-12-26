55411a587af54e5e1ad7f8a40d8b63a813fb8f3b
Leandro <leandro.wsp@gmail.com>
2023-12-26 19:04:31 -0300

    implemenado teste das classes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Instanciando as classes implementadas
+
+Nokia nokia = new Nokia("1199789129", "Nokia 5110", "353612656105272", 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Cybertron VR 6000");
+
+Console.WriteLine();
+
+Iphone iphone = new Iphone("1199789129", "iPhone 4 SE", "353612656106666", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Ultimate RAM Downloader");
+
+Console.WriteLine();
+
+Console.WriteLine("Teste finalizado");
